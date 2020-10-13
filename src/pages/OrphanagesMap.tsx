56f9e4fe44b3951a2fdbceb1e6afe4b2f2cd3495
Jefferson da Silva <je_jss@hotmail.com>
2020-10-13 04:08:59 -0300

    :star2: create orphanagemap page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import logoPointer from '../images/logoPointer.svg';
+
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
     return (
@@ -14,14 +19,28 @@ function OrphanagesMap() {
                     <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
 
-                <div className="map"></div>
-
-
                 <footer>
                     <strong>São Paulo</strong>
                     <span>São Paulo</span>
                 </footer>
             </aside>
+
+
+
+            < Map 
+                center={[-23.4413635,-46.7434585]}
+                zoom={15}
+                style={{ width: '100%', height: '100%'}}
+            >
+                < TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_TOKEN_MAPBOX}`}
+                />
+            </Map>
+
+
+            < Link to='' className='create-orphanage'>
+                <FiPlus size={32} color='#fff'/>
+            </Link> 
         </div>
     );
 }
